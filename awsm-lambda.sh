21e50cd8d45d45b22adc284c0a8de54cf808ae8b
Jacob Evans <jacob@dekz.net>
2016-07-07 17:46:20 +1000

    split out into sub commands

--- awsm-lambda.sh
+++ awsm-lambda.sh
@@ -1,37 +1,12 @@
+AWSM_LAMBDA_SOURCE_FILE=$(realpath ${BASH_SOURCE[0]})
+AWSM_LAMBDA_SOURCE_DIR=$(realpath `dirname ${AWSM_LAMBDA_SOURCE_FILE}`/)
+
+source $AWSM_LAMBDA_SOURCE_DIR/awsm-lambda-sh.sh
+
 function lambdas {
-  local query='
-    Functions[][
-      [
-        FunctionName,
-        Runtime
-      ]
-    ][]
-  '
-  aws lambda list-functions \
-    --output text           \
-    --query "$query"        |
-  sort                      |
-  column -s$'\t' -t
+  list
 }
 
-function lambda-invoke {
-  local payload="{}"
-  if [ ! -t 0 ] ; then payload=$(cat) ; fi
-  local lambda_line=$(lambdas | $FUZZY_FILTER)
-  local lambda_id=$(echo $lambda_line | read_inputs)
-
-  if [ -n "$lambda_id" ]; then
-    local out_file="/tmp/awsm-lambda.log"
-    aws lambda invoke --function-name $lambda_id \
-      --invocation-type RequestResponse \
-      --log-type Tail \
-      --payload "${payload:-'{}'}" \
-      $out_file > /dev/null
-    local status=$?
-
-    cat $out_file && rm $out_file
-
-    exit $status
-  fi
+function lambda {
+  $@
 }
-
